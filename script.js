60d2b88f31fd77b8e8bb398b86fbe153ca1f59d5
Abishek Anbazhagan <designer.abishek@gmail.com>
2024-03-25 15:01:24 +0530

    added budget

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
-var expenseValues = document.querySelectorAll(".expense_value");
-var incomeValues = document.querySelectorAll(".income_value");
 
-var totalExpenseValue = document.querySelector(".expense_total_value");
-var totalIncomeValue = document.querySelector(".income_total_value");
 
 // console.log(expenseValues);
 // console.log(incomeValues);
 // console.log(totalExpenseValue);
 // console.log(totalIncomeValue);
 
-var total = 0;
 
-expenseValues.forEach(sumOfExpenses);
+angular.module('myApp', [])
+.controller('myCtrl', ['$scope', function($scope) {
 
-function sumOfExpenses(item, index) {
-    total = total + Number(item.value);
-}
+    // Initializing the total
+    var total = 0;
 
-var app = angular.module('myApp', []);
-app.controller('myCtrl', function($scope) {
+    // Getting References to the necessary InputFields
+    var expenseValues = document.querySelectorAll(".expense_value");
+    var incomeValues = document.querySelectorAll(".income_value");
+    var totalExpenseValue = document.querySelector(".expense_total_value");
+    var totalIncomeValue = document.querySelector(".income_total_value");
+
+    // Running sumOfExpenses function on every expense InputField
+    $scope.ReCalculate = function(){
+        expenseValues.forEach(sumOfExpenses);
+
+        function sumOfExpenses(item, index) {
+        total = total + Number(item.value);
+        $scope.expense_total_value = total;
+        }
+    }
+
+    $scope.ReCalculate();
     $scope.expense_total_value = total;
-});
+}]);
